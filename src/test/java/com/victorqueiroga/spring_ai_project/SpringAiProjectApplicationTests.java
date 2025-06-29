5624caded3f44169a690c9bdb4cd6011862f2003
Victor Queiroga <victorqueiroga.jp@gmail.com>
2025-06-29 19:58:39 -0300

    first commit

---
+++ src/test/java/com/victorqueiroga/spring_ai_project/SpringAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorqueiroga.spring_ai_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
